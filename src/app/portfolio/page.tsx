4506313e21e825e395ae212b1b2fbba50b05f6d1
Pulkit0111 <ptyagi1551@gmail.com>
2025-03-05 20:47:28 +0530

    added chain wise data render on DOM

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -11,9 +11,12 @@ import { micah } from '@dicebear/collection';
 
 export default function Portfolio() {
     const router = useRouter();
-    const {isConnected, address} = useAccount();
+    const {isConnected, address, chain} = useAccount();
     const [avatarBase64, setAvatarBase64] = useState<string>('');
-    // const [balances, setBalances] = useState({});
+    const [data, setData] = useState<any>({});
+
+    let selectedChain = chain?.name.toLowerCase();
+
     useEffect(() => {
         const checkConnection = () => {
             if (!isConnected) {
@@ -56,6 +59,7 @@ export default function Portfolio() {
         try{
             const response = await axios.get(`http://localhost:8080/api/portfolio/${address}`)
             console.log(response.data)
+            setData(response.data)
         } catch(err) {
             console.error(err)
         }
@@ -67,14 +71,45 @@ export default function Portfolio() {
             <div className="fixed top-6 right-6 z-20">
                 <ConnectButton showBalance={false}/>
             </div>
-            <div className="flex flex-col items-center justify-center w-3/8 h-3/4 rounded-xl">
-                <div className="flex flex-row items-center justify-center w-[50%] h-[30%] mb-20">
+            <div className="flex flex-col items-center justify-center w-[80%] h-[80%] rounded-xl">
+                <div className="flex flex-row items-center justify-center w-[50%] h-[30%] mb-5">
                     <img src={`data:image/svg+xml;base64,${avatarBase64}`} alt="avatar"/>
                 </div>
-                <div className="flex flex-row items-center justify-center w-[90%] h-[50%] border-2 border-white rounded-xl">
-                    For Tokens
+                <div className="flex flex-col items-center justify-center w-full h-[80%] pl-10 pr-10">
+                    <>
+                        {Object.keys(data?.balances || {}).includes(selectedChain || '') ? (
+                            <>
+                                <div className="flex flex-row items-center justify-around w-[80%] h-[20%] border-2 border-white mt-2.5 mb-2.5 rounded-lg">
+                                    <p>{data.balances[selectedChain!].native.symbol}</p>
+                                    <p>{Number(data.balances[selectedChain!].native.balance).toFixed(2)} {data.balances[selectedChain!].native.symbol}</p>
+                                    <p>${Number(data.balances[selectedChain!].native.valueInUSD).toFixed(2)}</p>
+                                </div>
+                            </>
+                        ):(
+                            <>
+                            </>
+                        )}
+                        {Object.keys(data?.balances || {}).includes(selectedChain || '') ? (
+                            <>
+                                {data.balances[selectedChain!].erc20Tokens.map((token: any) => (
+                                    <div className="flex flex-row items-center justify-around w-[80%] h-[20%] border-2 border-white mt-2.5 mb-2.5 rounded-lg">
+                                        <p>{token.symbol}</p>
+                                        <p>{Number(token.balance).toFixed(2)} {token.symbol}</p>
+                                        <p>${Number(token.valueInUSD).toFixed(2)}</p>
+                                    </div>
+                                ))}
+                            </>
+                            ) : (
+                            <>
+                                <div className="flex flex-row items-center justify-around w-[80%] h-[20%] border-2 border-white mt-2.5 mb-2.5 rounded-lg">
+                                    <p>No Data for {selectedChain}</p>
+                                </div>
+                            </>
+                        )}
+                    </>
                 </div>
             </div>
+            
         </div>
     )
 }
